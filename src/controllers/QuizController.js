0c33e0fb3bb6dc05ef4c38678e2c9ecbac5ba632
unknown <inathan.moreira.10@gmail.com>
2020-06-24 16:38:20 -0300

    Sistema de CRUD de Quiz feito.

--- src/controllers/QuizController.js
+++ src/controllers/QuizController.js
@@ -5,5 +5,41 @@ module.exports = {
     async index(request, response) {
         const quizzes = await knex('quizzes');
         return response.json(quizzes);
+    },
+
+    async create(request, response) {
+
+        const {name, description, pointValue} = request.body
+
+        await knex('quizzes').insert({
+            name,
+            description,
+            pointValue
+        });
+
+        return response.status(201).send()
+    },
+
+    async update(request, response) {
+
+        const {name, description, pointValue} = request.body
+        const {id} = request.params
+
+        await knex('quizzes').update({
+            name,
+            description,
+            pointValue
+        }).where({id});
+
+        return response.send()
+    },
+
+    async delete(request, response) {
+
+        const {id} = request.params
+
+        await knex('quizzes').where({id}).del()
+
+        return response.send()
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -9,5 +9,8 @@ routes.get("/questions", QuestionController.index);
 routes.get("/questions/:quizId", QuestionController.show);
 
 routes.get("/quizzes", QuizController.index);
+routes.post("/quizzes", QuizController.create);
+routes.put("/quizzes/:id", QuizController.update);
+routes.delete("/quizzes/:id", QuizController.delete);
 
 module.exports = routes;
