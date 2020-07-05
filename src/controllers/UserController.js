c9469adc5009f9f3d748d402d296aaee2dffb386
Alisson-Henrique <torgatesousau@gamil.com>
2020-07-05 07:58:17 -0300

    finalizing user CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,54 @@
 const knex = require('../database/connection');
+const { response } = require('express');
 
 module.exports = {
     async index (request,response){
         const results = await knex('users')
 
         return response.json(results)
-    }
+    },
+    async create (request, require){
+        const { username, skips, points } = request.body
 
+        await knex('users').insert({
+            username,
+            skips,
+            points
+        });
+
+        return response.status(201).send();
+
+    },
+    async update(request, require, next){
+        try{
+            const {username, skips, points} = knex.body;
+            
+            await knex('users').update({
+                username,
+                skips,
+                points
+            })
+            return response.send()
+
+        }catch(error){
+            next(error)
+        }
+
+
+    },
+    async delete(request,require,next){
+        try{
+            const { id } = request.params
+
+            await knex("users").where({id}).del()
+
+            response.send()
+        }catch(error){
+            next(error)
+        }
+
+    }
+    
 }
+
+
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,9 @@ const QuizController = require('./controllers/QuizController');
 const UserController = require('./controllers/UserController');
 
 routes.get("/users", UserController.index);
+routes.post("/users",UserController.create);
+routes.put("/users",UserController.update);
+routes.delete("/users/:id",UserController.delete)
 
 routes.get("/questions", QuestionController.index);
 routes.get("/questions/:quiz_id", QuestionController.show);
